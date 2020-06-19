69d4fab45e1b17e7e830adbf5b3d48832e0902c9
TaylorDorsey1 <67131089+TaylorDorsey1@users.noreply.github.com>
2020-06-18 20:34:45 -0500

    Create CelsiustoFarenheit.c

---
+++ CelsiustoFarenheit.c
@@ -0,0 +1,22 @@
+*/ I need help creating a loop for this file to repeat code*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+float celsius, fahrenheit;
+{
+
+//Enter temperature in Celsius
+printf("Enter Temperature in Celsius : ");
+scanf("%f", &celsius);
+
+//Conversion formula for Celsius to Fahrenheit
+fahrenheit = (1.8 * celsius) + 32;
+
+//Display converted temperature in Fahrenheit
+printf("Converted Temperature in Fahrenheit : %f ", fahrenheit);
+
+for(fahrenheit =1. celsius<=100, fahrenheit = celsius + 1);
+
+return 0;
+}
